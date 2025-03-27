84e134893cfd2ef224617a4b66004da1bf7ac4dd
Tuhin492 <tuhinmondal492@gmail.com>
2025-03-27 20:46:59 +0530

    Add files via upload

---
+++ Computer general understanding.js
@@ -0,0 +1,31 @@
+const operatingsystem = [ // operating system (computer literacy)
+    { // 5
+        question: "Google acquired Android in?",
+        answers: ["2000", "2006", "2005", "2003"],
+        correct: "2005"
+    },    
+    { // 4
+        question: "What is open source?",
+        answers: ["It is license free", "It is usually free to use, but some services may be paid.", "The source code is freely available for modification and distribution.", "It encourages collaboration and community-driven development.", "All of the above"],
+        correct: "All of the above",
+        description: "Open-source.<br>Open means License free. Any platform either software or OS or others which are available to users with no cost.<br>Source means, its source code can be remodified or reused according to the users need."
+    },
+    { // 3
+        question: "What is/are the function(s) of Operating System (OS)?",
+        answers: ["OS is an software program that enables the hardware to communicate and operate with the software", "OS can handle process management, device management, Driver management, memory management etc.", "OS acts as a interface between user and hardware", "OS provide a software platform on top of which application programs can run", "All of the above" ],
+        correct: "All of the above"
+    },
+    { // 2
+        question: "What is the primary function of an Operating System or OS?",
+        answers: ["Managing computer hardware and software resources", "Running only application programs", "Converting high-level language to machine code", "Connecting the computer to the internet"],
+        correct: "Managing computer hardware and software resources",
+        description: "Electronic device contains roughly hardware and software. An operating system is works as a medium for establishing communication between the hardware and the software. Thus Its function is to take input through software and generate output through hardware.<br><br>User>Apps/Software>OS>Hardware<br><br>As an example in case of fire-alarm which is an electronic circuit continuously checks (takes input) the temperature of outside, and when the temperature is more than the set temperature set in the programming, it generate output through the hardware i.e., start the alarm."
+    },
+    { // 1
+        question: "What is an Android?",
+        answers: ["Operating System", "Software", "Application", "All of them"],
+        correct: "Operating System",
+        description: "Android is an open source Linux-based operating system"
+    },
+    
+]
