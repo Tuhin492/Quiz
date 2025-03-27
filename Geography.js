ec589e080198f928f8be0831079b3fb1b2b09c05
Tuhin492 <tuhinmondal492@gmail.com>
2025-03-27 21:15:38 +0530

    Create Geography.js

---
+++ Geography.js
@@ -0,0 +1,14 @@
+const geography = [
+{
+    question: "Mount Everest is located at",
+    answers: ["India", "Nepal", "Sri Lanka", "Bhutan"],
+    correct: "Nepal",
+    description: "Nepal which is present north to india contains the most height point on earth, Mount Everest"
+},
+{
+    question: "Earth is also known as:",
+    answers: ["Red planet", "Green planet", "Blue planet", "Black planet"],
+    correct: "Blue planet",
+    description: "Since 3/4 content of earth is covered with water, thus it is called blue planet"
+},
+]
