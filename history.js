c0fd9e3532cc45f7ee85baf7ad78b6d33dbd8deb
Tuhin492 <tuhinmondal492@gmail.com>
2025-03-27 21:00:20 +0530

    Create history.js

---
+++ history.js
@@ -0,0 +1,12 @@
+const history = [
+   {
+        question: "Is there any other nutrient besides iodine which salt is fortified with?",
+        answers: ["Calcium", "Iron", "Vitamin", "Vitamin D"],
+        correct: "Iron"
+    },
+    {
+        question: "Which GM variety of rice has enhanced beta carotenes and iron.",
+        answers: ["Brown Rice", "Golden Rice", "Green Rice", "None of these"],
+        correct: "Golden Rice"
+    },
+  ]
